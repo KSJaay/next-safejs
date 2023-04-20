a3b2a53afeec550514ff80c818c16a37d207c3fb
notksjaay <ksjaay@gmail.com>
2023-04-21 00:00:00 +0100

    Added new types

--- src/typings/cookie.d.ts
+++ src/typings/cookie.d.ts
@@ -1,3 +1,4 @@
+import {IncomingMessage, ServerResponse} from "http";
 export interface CookieAttributes {
   path?: string;
   domain?: string;
@@ -5,4 +6,39 @@ export interface CookieAttributes {
   sameSite?: "strict" | "Strict" | "lax" | "Lax" | "none" | "None" | true;
   secure?: boolean;
   [property: string]: any;
+}
+
+export interface GetCookie {
+  key: string;
+  request?: IncomingMessage;
+}
+
+export interface GetMultipleCookies {
+  request?: IncomingMessage;
+  keys: string[];
+}
+
+export interface SideCookieOptions {
+  options?: CookieAttributes;
+  hostname?: string;
+}
+
+export interface RemoveCookie {
+  key: string;
+  request?: IncomingMessage;
+  response?: ServerResponse;
+}
+
+export interface Serialize {
+  name: string;
+  val: string;
+  options?: CookieAttributes;
+}
+
+export interface SetCookie {
+  key: string;
+  data: any;
+  options?: CookieAttributes;
+  request?: IncomingMessage;
+  response?: ServerResponse;
 }
